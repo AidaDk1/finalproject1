0fce3efa7c0fe2a841d98e4f22649b4f1bae4c8d
AidaDk1 <65682383+AidaDk1@users.noreply.github.com>
2021-05-14 00:37:16 +0600

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -127,6 +127,8 @@ else {
         <h3 style="color: red">About the food culture in Le cafe'</h3>
         <br>
         <h3 style="color: white">Order food & beverages online from restaurants near & around you. <h3 style="color: white">We deliver food from your neighborhood local joints, your favorite cafes, luxurious & elite restaurants in your area,</h3><h3 style="color : white"> and also from chains like Dominos, KFC, Burger King, Pizza Hut, FreshMenu, Mc Donald's, Subway, Faasos, Cafe Coffee Day, Taco Bell, and more. Exciting bit?</h3><h3 style="color : white"> We place no minimum order restrictions! Order in as little (or as much) as you'd like. We'll Swiggy it to you!</h3></h3>
+   <p >This is the web application of  KyrgyzFood restaurant
+				,Where you can order any kind of food online.We deliver wide variety of foods  </p>
     </div>
 
       <iframe src="https://www.google.com/maps/place/Le+Cafe/@11.9316155,79.8335933,17z/data=!3m1!4b1!4m5!3m4!1s0x3a53636a24873e45:0x7f1e7ad6a30982dd!8m2!3d11.9316103!4d79.835782" style="width: 250px"></iframe>
